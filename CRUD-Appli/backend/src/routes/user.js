0fbbbefdf5610952feebd05290d03039e162f51c
Husnapawar <165997157+Husnapawar@users.noreply.github.com>
2025-09-06 12:08:19 +0530

    Update user.js

--- CRUD-Appli/backend/src/routes/user.js
+++ CRUD-Appli/backend/src/routes/user.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
+//const { searchUser } = require("../controllers/userController");
 const pool = require('../db');
 
 const { handleGetAllUsers,
      handleUpdateUserById ,
     handleDeleteUserById, 
-    handleCreateNewtUser
+    handleCreateNewtUser,
+   // searchUser
     } =  require("../controllers/user");
 // Get all users
 router.get('/',handleGetAllUsers); 
@@ -15,10 +17,12 @@ router.post('/',handleCreateNewtUser);
 
 // Delete a user
 router.delete('/:id',handleDeleteUserById);
- 
+
 // Update a user
 router.put('/:id',handleUpdateUserById);
- 
 
+//  router.get('/search',searchUser);
 
 module.exports = router;
+
+
